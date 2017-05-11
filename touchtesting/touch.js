42140483b5b7932567176357127c1a8e65ab3d3a
xuyuandehao <1148452777@qq.com>
2017-05-11 21:22:07 +0800

    add-touch-event

--- touchtesting/touch.js
+++ touchtesting/touch.js
@@ -262,7 +262,7 @@ function ongoingTouchIndexById(idToFind){
 					self.elem.style.width = selfWidth - changePoint2x*changePoint2x/2;
 					self.elem.style.height = selfHeight - changePoint2y*changePoint2y/2; 
 				}
-				var document.getElementById('log').innerText = touches[0].identifier+',' + touches[1].identifier+'\n';
+				var document.getElementById('log').innerText == touches[0].identifier+',' + touches[1].identifier+'\n';
 			};
 
 			function scaleEnd(evt){
