7ed39035c30b4ae88d39fb2bdcd74536d291aa75
xuyuandehao <1148452777@qq.com>
2017-05-12 21:39:11 +0800

    add-touch-event

--- snake/snake.js
+++ snake/snake.js
@@ -234,7 +234,7 @@ $(document).ready(function() {
 						return true;
 					};
 				};
-				$('#log').text((parseInt(this.boundingLeft) );
+				$('#log').text( ''+this.boundingLeft );
 				if( this.positionList[1].x == parseInt(this.boundingLeft)-this.size+'px'||
 					this.positionList[1].x == this.boundingRight ||
 					this.positionList[1].y == parseInt(this.boundingTop)-this.size+'px' ||
